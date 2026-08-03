0d0449276168fa0cb617ed139b761885b9ac9d28
prasanaseyon <75750776+prasanaseyon@users.noreply.github.com>
2026-08-03 11:45:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar, Footer, ScrollToTop } from "./layout";
 import {
   Home, About, Work, WorkDetail, Projects, Awards,
-  Publications, Volunteering, Skills, Contact, NotFound,
+  Publications, Volunteering, Skills, Sports, Contact, NotFound,
 } from "./pages";
 import "./styles.css";
 
@@ -24,6 +24,7 @@ export default function App() {
             <Route path="/publications" element={<Publications />} />
             <Route path="/volunteering" element={<Volunteering />} />
             <Route path="/skills" element={<Skills />} />
+            <Route path="/sports" element={<Sports />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
